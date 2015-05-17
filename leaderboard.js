f0ac70cbf25147ae8324a301463203c4e7564c31
Scott Davis <sc.ttdav.s@gmail.com>
2015-05-17 15:19:36 -0400

    init commit

---
+++ leaderboard.js
@@ -0,0 +1,39 @@
+PlayersList = new Mongo.Collection('players');
+
+if (Meteor.isClient) {
+  Template.leaderboard.helpers({
+    'player': function() {
+      return PlayersList.find({}, {sort: {score: -1, name: 1} })
+    },
+    'selectedClass': function(){
+      var playerId = this._id;
+      var selectedPlayer = Session.get('selectedPlayer');
+      if (playerId == selectedPlayer) {
+        return "selected"
+      }
+    },
+    'showSelectedPlayer': function(){
+      var selectedPlayer = Session.get('selectedPlayer');
+      return PlayersList.findOne(selectedPlayer);
+    }
+  });
+
+  Template.leaderboard.events({
+    'click .player': function(){
+      var playerId = this._id;
+      Session.set('selectedPlayer', playerId);
+    },
+    'click .increment': function(){
+      changeScore(5);
+    },
+    'click .decrement': function(){
+      changeScore(-5);
+    }
+  });
+
+  function changeScore(val) {
+    var selectedPlayer = Session.get('selectedPlayer');
+    PlayersList.update(selectedPlayer, {$inc: {score: val} });
+  }
+}
+
